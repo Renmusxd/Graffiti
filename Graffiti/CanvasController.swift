e2cd782d028843938ab184e74d7d24e0ac5e2dee
gmulz <themulitzer@gmail.com>
2014-12-24 22:43:49 -0500

    Canvas

---
+++ Graffiti/CanvasController.swift
@@ -0,0 +1,35 @@
+//
+//  CanvasController.swift
+//  Graffiti
+//
+//  Created by G Mulitz on 12/24/14.
+//  Copyright (c) 2014 RenmusLabs. All rights reserved.
+//
+
+import UIKit
+
+class CanvasController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
