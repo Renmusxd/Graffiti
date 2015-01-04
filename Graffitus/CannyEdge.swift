7a0224a04bef54b83a379815c446b033fb865f1f
Renmusxd <sumnernh@gmail.com>
2015-01-03 16:46:43 -0800

    Added opencv and started bridge

---
+++ Graffitus/CannyEdge.swift
@@ -0,0 +1,10 @@
+//
+//  CannyEdge.swift
+//  Graffitus
+//
+//  Created by Sumner Hearth on 1/3/15.
+//  Copyright (c) 2015 Blimp Development. All rights reserved.
+//
+
+import Foundation
+
---
+++ Graffitus/OpencvBridge.m
@@ -0,0 +1,10 @@
+//
+//  OpencvBridge.m
+//  Graffitus
+//
+//  Created by Sumner Hearth on 1/3/15.
+//  Copyright (c) 2015 Blimp Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Open
